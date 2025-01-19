151fa42e0ba1f28b066a40bdbd8972de4e68c470
woops425 <162817809+woops425@users.noreply.github.com>
2025-01-19 20:04:48 +0900

    Add files via upload
    DTO

---
+++ ArticleForm.java
@@ -0,0 +1,27 @@
+package com.example.first_project.dto;
+
+
+import com.example.first_project.entity.Article;
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
+}
