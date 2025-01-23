c7bfc784098ee9c017186179b714704810d83c2a
woops425 <162817809+woops425@users.noreply.github.com>
2025-01-23 16:08:53 +0900

    Add files via upload
    리팩토링을 거친 후의 ArticleForm 파일

--- ArticleForm.java
+++ ArticleForm.java
@@ -2,24 +2,16 @@ package com.example.first_project.dto;
 
 
 import com.example.first_project.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+// 리팩토링
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
