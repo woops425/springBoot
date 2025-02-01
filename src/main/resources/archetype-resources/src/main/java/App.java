62632ec450ac8737ec1f6dee8e50d0ac24ee1e76
woops425 <162817809+woops425@users.noreply.github.com>
2025-02-01 22:13:10 +0900

    Add files via upload

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $io.namoosori;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
